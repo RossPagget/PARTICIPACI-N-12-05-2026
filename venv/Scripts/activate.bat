cbb0b560fe4130755ffc737702dd33ae64445ed7
RossPagget <rospaye3@gmail.com>
2026-05-19 17:52:34 -0400

    Versiónl del sistema web

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\proyecto\Clinica_Medica\venv"
+set "VIRTUAL_ENV=F:\Documentos\Downloads\Clinica_Medica\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
